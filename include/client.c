79236f803bf66ec573c3f81cec47e74c6d8bf1e0
rsghatpa <rsghatpa@ncsu.edu>
2018-03-10 14:18:40 -0500

    Initial commit

--- include/client.c
+++ include/client.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
-#define PORT 8080
+//#define PORT 8080
   
 int main(int argc, char const *argv[]){
     struct sockaddr_in address;
@@ -22,7 +22,7 @@ int main(int argc, char const *argv[]){
     memset(&serv_addr, '0', sizeof(serv_addr));
   
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_port = htons(atoi(argv[1]));
       
     // Convert IPv4 and IPv6 addresses from text to binary form
     if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
