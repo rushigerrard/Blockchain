afe4ba49fdb0ea14e7852c25ff932b3a267f0ceb
Rushikesh Ghatpande <rsghatpa@ncsu.edu>
2018-03-18 12:18:09 -0400

    /arrival API added for new node

--- include/MinerServer.cpp
+++ include/MinerServer.cpp
@@ -1,7 +1,7 @@
 /* MinerServer.cc
    Rushikesh Ghatpande, February 15, 2018
-   
-   
+
+
 */
 
 #include <pistache/net.h>
@@ -10,14 +10,13 @@
 #include <pistache/http_headers.h>
 #include <pistache/cookie.h>
 #include <pistache/endpoint.h>
-
+#include "reader.h"
 using namespace std;
 using namespace Pistache;
 
 void printUsage(){
-	printf("./MinerServer [port_no] [no_of_threads]\n");
+        printf("./MinerServer [port_no] [no_of_threads]\n");
 }
-
 struct PrintException {
     void operator()(std::exception_ptr exc) const {
         try {
@@ -36,7 +35,7 @@ class MyHandler : public Http::Handler {
             const Http::Request& req,
             Http::ResponseWriter response) {
 
-		if (req.resource() == "/ping") {
+                if (req.resource() == "/ping") {
             if (req.method() == Http::Method::Get) {
 
                 using namespace Http;
@@ -70,27 +69,66 @@ class MyHandler : public Http::Handler {
                 response.send(Http::Code::Method_Not_Allowed);
             }
         }
-		else if (req.resource() == "/transaction"){
-			if(req.method() == Http::Method::Post){
-				response.send(Http::Code::Ok, req.body(), MIME(Text, Plain));
-			}else{
-				response.send(Http::Code::Ok, req.body(), MIME(Text, Plain));
-			}
-		}
-		else if(req.resource() == "/solved_block"){
-			if(req.method() == Http::Method::Post){
-				response.send(Http::Code::Ok, req.body(), MIME(Text, Plain));
-			}else{
-				response.send(Http::Code::Ok, req.body(), MIME(Text, Plain));
-			}
-		}
+                else if (req.resource() == "/transaction"){
+                        if(req.method() == Http::Method::Post){
+                                response.send(Http::Code::Ok, req.body(), MIME(Text, Plain));
+                        }else{
+                                response.send(Http::Code::Ok, req.body(), MIME(Text, Plain));
+                        }
+                }
+                else if(req.resource() == "/solved_block"){
+                        if(req.method() == Http::Method::Post){
+                                response.send(Http::Code::Ok, req.body(), MIME(Text, Plain));
+                        }else{
+                                response.send(Http::Code::Ok, req.body(), MIME(Text, Plain));
+                        }
+                }
         else if (req.resource() == "/static") {
             if (req.method() == Http::Method::Get) {
                 Http::serveFile(response, "README.md").then([](ssize_t bytes) {;
                     std::cout << "Sent " << bytes << " bytes" << std::endl;
                 }, Async::NoExcept);
             }
-        } else {
+        }
+                else if (req.resource() == "/arrival"){
+			cout<<"New node arrived"<<endl;
+                        if(req.method() == Http::Method::Post){
+
+                                using namespace Http;
+
+                auto query = req.query();
+                if (query.has("chunked")) {
+                    std::cout << "Using chunked encoding" << std::endl;
+
+                    response.headers()
+                        .add<Header::Server>("pistache/0.1")
+                        .add<Header::ContentType>(MIME(Text, Plain));
+
+                    response.cookies()
+                        .add(Cookie("lang", "en-US"));
+
+                    auto stream = response.stream(Http::Code::Ok);
+                        /*
+                    vector<string> broadcast_list = read_broadcast_list();
+                                        for(int i = 0; i < broadcast_list.size(); i++){
+                                                stream << broadcast_list.at(i);
+                                        }
+                        */
+                                        stream << ends;
+                }
+                else {
+                                        string share_broadcast_string = "";
+                                        vector<string> broadcast_list = read_broadcast_list();
+                                        for(int i = 0; i < broadcast_list.size(); i++){
+                                                share_broadcast_string = share_broadcast_string + broadcast_list.at(i);
+                                        }
+                                        response.send(Http::Code::Ok, share_broadcast_string);
+                }
+                        }else{
+                                response.send(Http::Code::Ok, req.body(), MIME(Text, Plain));
+                        }
+                }
+                else {
             response.send(Http::Code::Not_Found);
         }
 
@@ -104,17 +142,17 @@ class MyHandler : public Http::Handler {
 
 };
 int main(int argc, char *argv[]) {
-	
-	//Default port is 9080
+
+        //Default port is 9080
     Port port(9080);
 
     int thr = hardware_concurrency();
 
-	if(argc >= 4){
-		printUsage();
-		exit (EXIT_FAILURE);
-	}
-	//user can define the port and number of concurrent threads;
+        if(argc >= 4){
+                printUsage();
+                exit (EXIT_FAILURE);
+        }
+        //user can define the port and number of concurrent threads;
     if (argc >= 2) {
         port = std::stol(argv[1]);
 
@@ -128,7 +166,7 @@ int main(int argc, char *argv[]) {
     cout << "Cores = " << hardware_concurrency() << endl;
     cout << "Using " << thr << " threads" << endl;
 
-	//Here server is a pointer to HTTP::Endpoint addr
+        //Here server is a pointer to HTTP::Endpoint addr
     auto server = std::make_shared<Http::Endpoint>(addr);
 
     auto opts = Http::Endpoint::options()
