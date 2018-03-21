9c6a0929b5784d80ec134b652dd1fb1f8bfee67d
Abhash Jain <ajain28@ncsu.edu>
2018-03-21 19:56:09 -0400

    Tx updated

--- include/tx.h
+++ include/tx.h
@@ -2,22 +2,43 @@
 #define BC_TX_H
 #include <ostream>
 #include<vector>
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/base_object.hpp>
 using namespace std;
 
 class Tx{
 public:
-	Tx(string sender,string receiver,int data);
+	Tx(string sender,string receiver,int amount);
+	Tx();
+	Tx(string sender, string receiver, vector<string> inputTx, int amount);
+	string getId();
 	string getSender();
 	string getReceiver();
-	int getData();
+	int getAmount();
 	string toString();
+	vector<string> getInputs();
 	friend ostream& operator<<(ostream &strm, const Tx &tx);
+	friend class boost::serialization::access;
+	template<class Archive>
+	void serialize(Archive & ar, const unsigned int version){
+		ar & TxId;
+		ar & sender;
+		ar & receiver;
+		ar & inputTx;
+		ar & amount;
+		ar & leftoverAmt;
+	}
 private:
-	string Txid;
+	string TxId;
 	string sender;
 	string receiver;
-	vector<string> LinkedTx;
-	int data;
+	vector<string> inputTx;
+	int amount;
 	int leftoverAmt;
 };
 #endif
--- src/tx.cpp
+++ src/tx.cpp
@@ -5,9 +5,22 @@
 using namespace std;
 
 Tx::Tx(string sender, string receiver,int amount){
+	const long double sysTime = time(0);
+    this->TxId = sender + receiver + to_string(sysTime);
 	this->sender = sender;
 	this->receiver = receiver;
-	this->data= amount;
+	this->amount= amount;
+	this->leftoverAmt =-1;
+}
+
+Tx::Tx(string sender, string receiver,vector<string> inputs, int amount){
+	const long double sysTime = time(0);
+	this->TxId = sender + receiver + to_string(sysTime);
+	this->sender = sender;
+	this->receiver = receiver;
+	this->inputTx = inputs;
+	this->amount = amount;
+	this->leftoverAmt = -1;
 }
 
 string Tx::getSender(){
@@ -16,18 +29,26 @@ string Tx::getSender(){
 string Tx::getReceiver(){
 	return this->receiver;
 }
-int Tx::getData(){
-	return this->data;
+int Tx::getAmount(){
+	return this->amount;
+}
+
+string Tx::getId(){
+	return this->TxId;
+}
+
+vector<string> Tx::getInputs(){
+	return this->inputTx;
 }
 
 ostream& operator<<(ostream &strm,const Tx &tx){
-	return strm << tx.sender << "->" << tx.receiver << "->" << tx.data <<endl;
+	return strm << tx.sender << "->" << tx.receiver << "->" << tx.amount <<endl;
 }
 
 string Tx::toString(){
 	//ostringstream ss;
 	//ss<<strm.rdbuf();
 	//return ss.str();
-	return (this->sender + "->" + this->receiver +"->" + to_string(this->data) + "\n");
+	return (this->sender + "->" + this->receiver +"->" + to_string(this->amount) + "\n");
 }
 
