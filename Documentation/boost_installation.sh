ede9289c8f7ed2d1bca7b914e4138787fb2fbf42
Rushikesh Ghatpande <rsghatpa@ncsu.edu>
2018-04-12 17:29:00 -0400

    Some changes to .gitignore

---
+++ Documentation/boost_installation.sh
@@ -0,0 +1,6 @@
+#Boost installation:
+sudo apt-get install libboost-all-dev
+
+#Addding firewall setting in iptables to allow to start pisthach server:
+#sudo iptables -A vcl-inuse -p tcp -s <ip of node which you want to allow> -j ACCEPT
+#iptables save
