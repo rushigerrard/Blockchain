a861f3bfd2c3955771fbf0b1a26e0bf14397b271
ajain28 <ajain28@ncsu.edu>
2018-04-14 20:44:35 -0400

    Update boost_installation.sh

--- Documentation/boost_installation.sh
+++ Documentation/boost_installation.sh
@@ -3,4 +3,4 @@ sudo apt-get install libboost-all-dev
 
 #Addding firewall setting in iptables to allow to start pisthach server:
 #sudo iptables -A vcl-inuse -p tcp -s <ip of node which you want to allow> -j ACCEPT
-#iptables save
+#iptables-save
